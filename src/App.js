aa0f7949b33a04cac14be8730d164923db245af1
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-12-28 14:19:44 +1100

    added useEffect initial data fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import { useState, useEffect } from 'react'
 import Header from './Header'
 import HomePage from './HomePage.js'
 import TodoDashboard from './TodoDashboard'
 import TodoCalendar from './TodoCalendar.js'
 
 function App() {
+  const [allTodos, setAllTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/allTodos")
+    .then(r => r.json())
+    .then(data => {
+      const parsedEvents = data.map(item => ({
+        ...item,
+        start: new Date(item.start),
+        end: new Date(item.end)
+      }));
+      setAllTodos(parsedEvents)
+    });
+  }, [])
+  
+
   return (
     <div>
       <Header />
